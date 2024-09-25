2502618d337d400b480ecc19fad6ad589bf14251
Ali Mohseni <ali.mohseni05@yahoo.se>
2024-09-25 14:16:22 +0200

    routing and HTTP verbs

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const app = express();
 
+const db = [
+  {
+    id: 1,
+    name: 'Ali Mohseni',
+    email: 'ali.mohseni@appliedtechnology.se',
+  },
+  {
+    id: 1,
+    name: 'Mohseni Ali',
+    email: 'testingSomething@nothing.com',
+  },
+];
+
 app.get('/', (req, res) => {
   const devs = {
     id: 1,
     name: 'Ali Mohseni',
     email: 'ali.mohseni@appliedtechnology.se',
   };
-  res.status(201).setHeader('Location', `/api/developers/1`).json(devs);
+  res.status(201).setHeader('Location', `/api/developers/1`).json(db[1]);
 });
 
 const port = 3000;
