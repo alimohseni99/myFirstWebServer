05a90100238ab92a43ab54927b5633489e78b0d5
Ali Mohseni <ali.mohseni05@yahoo.se>
2024-09-25 12:36:41 +0200

    request

--- index.js
+++ index.js
@@ -2,12 +2,25 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  const content = fs.readFileSync('./static/index.html', 'utf-8');
+  const fileName = fileNameOfUrl(req.url);
+  const content = fs.readFileSync(`./static/${fileName}`, 'utf-8');
 
   res.statusCode = 200;
   res.setHeader('Content-type', 'text/html');
   res.end(content);
+
+  console.log(`The URL for the request was '${req.url}'`);
+  console.log(`The Method for the request was '${req.method}'`);
 });
+const fileNameOfUrl = (url) => {
+  let fileName = '';
+  if (url.split('/')[1] === '') {
+    fileName = 'index.html';
+  } else {
+    fileName = url.split('/')[1];
+  }
+  return fileName;
+};
 
 const hostName = 'localhost';
 const port = 3000;
