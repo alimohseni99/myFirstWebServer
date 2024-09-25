6a9965e4ee9353934b34974b420420734ed16d26
Ali Mohseni <ali.mohseni05@yahoo.se>
2024-09-26 00:52:41 +0200

    post

--- index.js
+++ index.js
@@ -8,16 +8,23 @@ const db = [
     email: 'ali.mohseni@appliedtechnology.se',
   },
   {
-    id: 1,
+    id: 2,
     name: 'Mohseni Ali',
     email: 'testingSomething@nothing.com',
   },
 ];
 
+app.use(express.json());
+
 app.get('/api/developers/:id', (req, res) => {
   const dev = db.find((dev) => dev.id == req.params.id);
 
   return dev ? res.json(dev) : res.status(404).end();
+});
+
+app.post('/api/developers/', (req, res) => {
+  console.log(req.body);
+  res.status(201).end();
 });
 
 const port = 3000;
