25c4afec971f799c7cd81f39b93744fbe7b75287
Ali Mohseni <ali.mohseni05@yahoo.se>
2024-09-25 12:17:18 +0200

    first webserver

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('started');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-typw', 'text/plain');
+  res.end('hello fellow developer!');
+});
+
+const hostName = 'localhost';
+const port = 3000;
+
+server.listen(port, hostName, () => {
+  console.log(`server runing at HTTP://'${hostName}:${port}/`);
+});
